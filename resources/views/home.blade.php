6ceb7f1df65ff75340108e8ff29be591ab23bfdb
Ulises R <uli.rp1999@gmail.com>
2021-06-28 23:15:01 -0600

    Prueba slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
         <!-- Header -->
-        <header id="header" class="header">
-            <div class="header-content">
+        <header id="header" class="slider">
+            <div class="slider-content">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-12">
