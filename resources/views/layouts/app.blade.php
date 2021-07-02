ba12aa42845a28541b7656a3e1ba80d1ddb0000b
Fernando Sandoval <sandovalf193@gmail.com>
2021-07-02 02:12:55 -0600

    footer responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -248,7 +248,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <p class="p-small">Copyright © 2020 <a href="#m">LichoDevelopment</a></p>
+                    <p class="p-small text-center">Copyright © 2020 <a href="#m">LichoDevelopment</a></p>
                 </div> <!-- end of col -->
             </div> <!-- enf of row -->
         </div> <!-- end of container -->
