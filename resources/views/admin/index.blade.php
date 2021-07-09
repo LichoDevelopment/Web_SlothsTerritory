00f40f8f078dc80b77d222e29268051e08f1de6f
UlisesRodriguezPerez <47537584+UlisesRodriguezPerez@users.noreply.github.com>
2021-07-09 00:14:09 -0600

    Cambios css

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,6 @@
 
 @section('content')
     <h2 class="display-4">Inicio</h2>
-    <a href="">Agencias</a>
     <a href="{{ route('agencias.index') }}" class="btn btn-success"> Ver Agencias</a>
  
 @endsection
