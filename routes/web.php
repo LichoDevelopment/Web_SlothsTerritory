e016457deee13edf0c688eec9f87b3b93008b730
UlisesRodriguezPerez <47537584+UlisesRodriguezPerez@users.noreply.github.com>
2021-07-11 15:09:05 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 /**Public routes */
 
-Route::redirect('/', '/es');
+Route::redirect('/', '/en');
 
 // Route::middleware('languageSwitcher')->group(['prefix' => '{language}'],function () {
 
