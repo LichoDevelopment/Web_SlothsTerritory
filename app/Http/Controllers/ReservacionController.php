85febc9fefe29f9b8d9f4a1eccf115ea8d7cf797
Fernando Sandoval <sandovalf193@gmail.com>
2021-07-24 20:57:13 -0600

    se quito la validacion para el maximo de paxó

--- app/Http/Controllers/ReservacionController.php
+++ app/Http/Controllers/ReservacionController.php
@@ -51,26 +51,26 @@ class ReservacionController extends Controller
        }
 
        $registro = Registro::where('id_horario',$this->request->id_horario)->first();
-       $horario = Horario::find($this->request->id_horario);
+    //    $horario = Horario::find($this->request->id_horario);
 
        $total_pax_en_reserva = $this->request->cantidad_adultos + $this->request->cantidad_niños + $this->request->cantidad_niños_gratis;
 
-       if($registro && ( $registro->cantidad_reservas + $total_pax_en_reserva ) > $horario->capacidad_maxima){
-           $capacidad_libre = $horario->capacidad_maxima - $registro->cantidad_reservas;
-           return redirect('/agregar_reserva')->with([
-               'limite' => 'Se ha exedido el limite para esta hora',
-               'capacidad_libre' => $capacidad_libre,
-               'capacidad_maxima' => $horario->capacidad_maxima,
-            ]);
-       }
+    //    if($registro && ( $registro->cantidad_reservas + $total_pax_en_reserva ) > $horario->capacidad_maxima){
+    //        $capacidad_libre = $horario->capacidad_maxima - $registro->cantidad_reservas;
+    //        return redirect('/agregar_reserva')->with([
+    //            'limite' => 'Se ha exedido el limite para esta hora',
+    //            'capacidad_libre' => $capacidad_libre,
+    //            'capacidad_maxima' => $horario->capacidad_maxima,
+    //         ]);
+    //    }
 
-       if(!$registro && $total_pax_en_reserva > $horario->capacidad_maxima){
-            return redirect('/agregar_reserva')->with([
-                'limite' => 'Se ha exedido el limite para esta hora',
-                'capacidad_libre' => $horario->capacidad_maxima,
-                'capacidad_maxima' => $horario->capacidad_maxima
-             ]);
-       }
+    //    if(!$registro && $total_pax_en_reserva > $horario->capacidad_maxima){
+    //         return redirect('/agregar_reserva')->with([
+    //             'limite' => 'Se ha exedido el limite para esta hora',
+    //             'capacidad_libre' => $horario->capacidad_maxima,
+    //             'capacidad_maxima' => $horario->capacidad_maxima
+    //          ]);
+    //    }
 
        
        $validator = Validator::make($this->request->all(), $this->reglasValidacion, $this->mensajesValidacion);
@@ -141,20 +141,20 @@ class ReservacionController extends Controller
        }
 
        $reserva = Reserva::find($id);
-       $horario = Horario::find($this->request->id_horario);
+    //    $horario = Horario::find($this->request->id_horario);
        $registro = Registro::where('id_horario',$this->request->id_horario)->first();
        $cantidad_previa_pax_en_reserva = $reserva->cantidad_adultos + $reserva->cantidad_niños + $reserva->cantidad_niños_gratis;
        $cantidad_reservas = $registro->cantidad_reservas - $cantidad_previa_pax_en_reserva;
 
        $total_pax_en_reserva = $this->request->cantidad_adultos + $this->request->cantidad_niños + $this->request->cantidad_niños_gratis;
 
-       if(( $cantidad_reservas + $total_pax_en_reserva ) > $horario->capacidad_maxima){
-           return redirect()->back()->with('limite', 'Se ha exedido el limite para esta hora');
-       }
+    //    if(( $cantidad_reservas + $total_pax_en_reserva ) > $horario->capacidad_maxima){
+    //        return redirect()->back()->with('limite', 'Se ha exedido el limite para esta hora');
+    //    }
 
-       if($total_pax_en_reserva > $horario->capacidad_maxima){
-            return redirect()->back()->with('limite', 'Se ha exedido el limite para esta hora');
-       }
+    //    if($total_pax_en_reserva > $horario->capacidad_maxima){
+    //         return redirect()->back()->with('limite', 'Se ha exedido el limite para esta hora');
+    //    }
 
        $validator = Validator::make($this->request->all(), $this->reglasValidacion, $this->mensajesValidacion);
 
