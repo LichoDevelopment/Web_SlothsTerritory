52ff18c46cfa1fedcdf9def15cd8ba8b3f11af38
UlisesRodriguezPerez <47537584+UlisesRodriguezPerez@users.noreply.github.com>
2021-12-08 23:04:21 -0600

    Update sliderCircular.js

--- public/js/sliderCircular.js
+++ public/js/sliderCircular.js
@@ -1,13 +1,4 @@
-/**
- * demo.js
- * https://coidea.website
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2018, COIDEA
- * https://coidea.website
- */
+
 
   $('.slideshow').imagesLoaded({ background: true })
   .done( function() {
