872318761722547a958c8e0105aadd84bdeab08a
Fernando Sandoval <sandovalf193@gmail.com>
2021-12-11 15:20:40 -0600

    combo modal agregado

--- resources/views/combo/combo.blade.php
+++ resources/views/combo/combo.blade.php
@@ -5,7 +5,7 @@
     <section class="wrapper-combo">
         <div class="container-fostrap-combo">
             <div>
-                <img src="/images/favicon.png" class="fostrap-logo-combo"/>
+                {{-- <img src="/images/favicon.png" class="fostrap-logo-combo"/> --}}
                 <h1 class="heading-combo">
                     Our Combos 
                 </h1>
@@ -34,115 +34,91 @@
                                                 <a href="#"> {{$combo->name}}
                                             </a>
                                             </h4>
-                                            <p class="">
-                                                {{$combo->description}}
+                                            <p class="preview-description" data-content="{{$combo->description}}">
+                                                
                                             </p>
                                         </div>
-                                        <div class="card-read-more-combo">
+                                        {{-- <div class="card-read-more-combo">
                                             <a href="#" class="btn btn-link btn-block">
                                                 Read More
                                             </a>
-                                        </div>
+                                        </div> --}}
+                                        <a class="popup-with-move-anim" href="#{{$combo->uuid}}"><div class="btn-solid-reg page-scroll">{{__('btn.mas_detalles')}}</div></a> 
                                     </div>
                                 </div>  
+
+                                <div id="{{$combo->uuid}}" class="lightbox-basic zoom-anim-dialog mfp-hide">
+                                    <div class="row justify-content-center">
+                                        <button title="Close (Esc)" type="button" class="mfp-close x-button">×</button>
+                                        <div class="col-lg-10 mb-2">
+                                            <img class="img-fluid" src="/storage/{{$combo->image}}" alt="tour de perezosos en La Fortuna" title="Tour de perezosos">
+                                        </div> <!-- end of col -->
+                                        <div class="col-lg-10">
+                                            <h3>{{$combo->name}}</h3>
+                                            {{-- <hr class="line-heading"> --}}
+                                            <hr>
+                                            <h6>{{$combo->description}}</h6>                                           
+
+                                            <div class="row mb-3">
+                                                <div class="col-lg-6 col-sm-12">
+                                                    <p>Itinirery</p>
+                                                    <ul class="list-group itinerary" data-itinerary="{{$combo->itinerary}}"></ul>
+                                                </div>
+                                                <div class="col-lg-6 col-sm-12">
+                                                    <p>Rates</p>
+                                                    <p>Adults Price: {{$combo->adult_price}}</p>
+                                                    <p>Kids Price:  {{$combo->kid_price}}</p>
+                                                </div>
+                                            </div>
+                                            <br>
+                                            <div class="row mb-2">
+                                                <div class="col-lg-6 col-sm-12">
+                                                    <p>What is Included:</p>
+                                                    <p>{{$combo->includes}}</p>
+                                                </div>
+                                                <div class="col-lg-6 col-sm-12">
+                                                    <p>What to bring: </p>
+                                                    <p>{{$combo->requirements}}</p>
+                                                </div>
+
+                                            </div>
+                                            <a class="btn-solid-reg" href="https://wa.me/message/UAO3TORZITGBE1" target="_blank">{{__('btn.reservar_ahora')}}</a> <a class="btn-outline-reg mfp-close as-button" href="#services">{{__('btn.volver')}}</a>
+                                        </div> <!-- end of col -->
+                                    </div> <!-- end of row -->
+                                </div> <!-- end of lightbox-basic -->
+                                <!-- end of lightbox -->
                         @endforeach
-                        
-                        {{-- <div class="col-xs-12 col-sm-4">
-                            <div class="card-combo">
-                                <a class="img-card-combo" href="#">
-                                <img src="\images\combos\ATV-La_Fortuna.png" />
-                            </a>
-                                <div class="card-content-combo">
-                                    <h4 class="card-title-combo">
-                                        <a href="#"> Sloth’s and ATV COMBO tour
-                                    </a>
-                                    </h4>
-                                    <p class="">
-                                        This is a combination of adrenaline and conexion with the nature will begin  with  
-                                        ATV it Will be a drive for about 4 hours by los Altos de Monterrey and la guaria area, we ...
-                                    </p>
-                                </div>
-                                <div class="card-read-more-combo">
-                                    <a href="#" class="btn btn-link btn-block">
-                                        Read More
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-4">
-                            <div class="card-combo">
-                                <a class="img-card-combo" href="#">
-                                <img src="\images\combos\cañon-tour-La_Fortuna.png" />
-                            </a>
-                                <div class="card-content-combo">
-                                    <h4 class="card-title-combo">
-                                        <a href="#">Sloths and Maquique Canyoning Tour
-                                    </a>
-                                    </h4>
-                                    <p class="">
-                                        The canyoning has 5 rappels where the lowest one is about 20 mts and higgest is anout 55mts
-                                        and one canopy line ...
-                                    </p>
-                                </div>
-                                <div class="card-read-more-combo">
-                                    <a href="#" class="btn btn-link btn-block">
-                                        Read More
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-4">
-                            <div class="card-combo">
-                                <a class="img-card-combo" href="#">
-                                <img src="\images\combos\cañon-tour-La_Fortuna.png" />
-                            </a>
-                                <div class="card-content-combo">
-                                    <h4 class="card-title-combo">
-                                        <a href="#">Sloths and Maquique Canyoning Tour
-                                    </a>
-                                    </h4>
-                                    <p class="">
-                                        The canyoning has 5 rappels where the lowest one is about 20 mts and higgest is anout 55mts
-                                        and one canopy line ...
-                                    </p>
-                                </div>
-                                <div class="card-read-more-combo">
-                                    <a href="#" class="btn btn-link btn-block">
-                                        Read More
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-4">
-                            <div class="card-combo">
-                                <a class="img-card-combo" href="#">
-                                <img src="\images\combos\cañon-tour-La_Fortuna.png" />
-                            </a>
-                                <div class="card-content-combo">
-                                    <h4 class="card-title-combo">
-                                        <a href="#">Sloths and Maquique Canyoning Tour
-                                    </a>
-                                    </h4>
-                                    <p class="">
-                                        The canyoning has 5 rappels where the lowest one is about 20 mts and higgest is anout 55mts
-                                        and one canopy line ...
-                                    </p>
-                                </div>
-                                <div class="card-read-more-combo">
-                                    <a href="#" class="btn btn-link btn-block">
-                                        Read More
-                                    </a>
-                                </div>
-                            </div>
-                        </div> --}}
                     </div>
                 </div>
             </div>
         </div>
     </section>
 
+@endsection
 
+@section('scripts')
+    <script>
+        const itineraries = document.querySelectorAll('.itinerary');
+        const previewDescription = document.querySelectorAll('.preview-description');
 
+        itineraries.forEach(itinerary => {
+            let itineraryArray = JSON.parse(itinerary.getAttribute('data-itinerary'));
+
+            itinerary.innerHTML = '';
+
+            itineraryArray.forEach(item => {
+                itinerary.innerHTML += `<li class="list-group-item">${item}</li>`;
+            });
+        });
+
+        previewDescription.forEach(preview => {
+            let content = preview.getAttribute('data-content');
+
+            let contentArray = content.split(' ');
+
+            preview.innerHTML = contentArray.slice(0, 20).join(' ') + '...';
+        });
+    </script>
 @endsection
 
 
