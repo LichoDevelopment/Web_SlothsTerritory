be15bd7273bae3a5ac931cda1a87b64cdfe4146a
Fernando Sandoval <sandovalf193@gmail.com>
2022-11-03 19:42:55 -0600

    update from server

--- resources/views/salesAgent/sales_agent.blade.php
+++ resources/views/salesAgent/sales_agent.blade.php
@@ -2,9 +2,9 @@
 
 
 @section('content')
-    <div id="sales-agent-container">
+    <div class="sales-agent-container">
         <div id="img-sales-agent-container">
-            <img id="img-sales-agent" src="images/perezosos/tierno-oso-perezoso-en-la-fortuna-sloths-territory.jpeg"
+            <img id="img-sales-agent" src="images/perezosos/oso-perezoso-con-hojas.jpeg"
                 alt="SlothsTerritory">
         </div>
 
@@ -51,4 +51,69 @@
 
         </div>
     </div>
+
+    <div class="container pd-agents" >
+        <div>
+            <h2 class="h2-sales-agent-info">INFORMACIÓN DE CONTACTO</h2>
+        </div>
+        <div class="sales-agent-grid">
+            <article class="sales-agent-grid-article">
+                <div >
+                    <h4 class="pd-bottom">RESERVACIONES</h4>
+                    <ul class="list-unstyled li-space-lg greenSloth">
+                        <li>
+                            <i class="fas fa-user"></i>
+                            <a class="agent-link" href="#">Juan José Pérez Rojas</a>
+                        </li>
+                        <li >
+                            <i class="fas fa-phone"></i>
+                            <a class="agent-link" href="tel:+50685610404">+506 8561-0404</a>
+                        </li>
+                        <li >
+                            <i class="fas fa-envelope"></i>
+                            <a class="agent-link" href="mailto:info@slothsterritory.com">info@slothsterritory.com</a>
+                        </li>
+                    </ul>
+                </div>
+            </article>
+            <article class="sales-agent-grid-article">
+                <div >
+                    <h4 class="pd-bottom">MERCADEO</h4>
+                    <ul class="list-unstyled li-space-lg greenSloth">
+                        <li>
+                            <i class="fas fa-user"></i>
+                            <a class="agent-link" href="#">Natalia Valenciano</a>
+                        </li>
+                        <li >
+                            <i class="fas fa-phone"></i>
+                            <a class="agent-link" href="tel:+50687069029">+506 8706-9029</a>
+                        </li>
+                        <li >
+                            <i class="fas fa-envelope"></i>
+                            <a class="agent-link" href="mailto:mercadeo@slothsterritory.com">mercadeo@slothsterritory.com</a>
+                        </li>
+                    </ul>
+                </div>
+            </article>
+            <article class="sales-agent-grid-article">
+                <div >
+                    <h4 class="pd-bottom">GERENCIA</h4>
+                    <ul class="list-unstyled li-space-lg greenSloth">
+                        <li>
+                            <i class="fas fa-user greenSloth"></i>
+                            <a class="agent-link" href="#">Keilor Rodríguez</a>
+                        </li>
+                        <li >
+                            <i class="fas fa-phone"></i>
+                            <a class="agent-link" href="tel:+50684119219">+506 8411-9219</a>
+                        </li>
+                        <li >
+                            <i class="fas fa-envelope"></i>
+                            <a class="agent-link" href="mailto:gerencia@slothsterritory.com">gerencia@slothsterritory.com</a>
+                        </li>
+                    </ul>
+                </div>
+            </article>
+        </div> <!-- end of row -->
+    </div> <!-- end of container -->
 @endsection
