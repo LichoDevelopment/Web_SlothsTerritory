1261a9ef1a2df09d331a82e8b4b4ddf6825c1656
UlisesRodriguezPerez <uli.rp1999@gmail.com>
2024-10-15 02:45:01 -0600

    refactor: Update daily notification schedule for transportation

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('transporte:enviar-notificaciones')->dailyAt('00:20');
+        $schedule->command('transporte:enviar-notificaciones')->dailyAt('22:00');
     }
 
     /**
